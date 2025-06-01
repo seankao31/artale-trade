11dfe2c268654dd55f5c1ce6442a133251b49d6c
Sean Kao <seankao31@gmail.com>
2025-05-31 20:32:11 -0700

    feat: basic listings page layout
    Signed-off-by: Sean Kao <seankao31@gmail.com>

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,28 @@
 <script lang="ts">
 	import '../app.css';
+	import { AppBar } from '@skeletonlabs/skeleton-svelte';
+
+	import CircleUser from '@lucide/svelte/icons/circle-user';
 
 	let { children } = $props();
 </script>
 
-{@render children()}
+<div class="grid h-screen grid-rows-[auto_1fr_auto]">
+	<AppBar>
+		{#snippet lead()}
+			<a href="/" class="h5">Artale Trade</a>
+		{/snippet}
+		{#snippet trail()}
+			<a href="/listings">Listings</a>
+			<CircleUser size={25} />
+		{/snippet}
+	</AppBar>
+	<div class="grid grid-cols-1 md:grid-cols-[auto_1fr_auto]">
+		<aside class="p-4">(ad-left)</aside>
+		<main class="p-4">
+			{@render children()}
+		</main>
+		<aside class="p-4">(ad-right)</aside>
+	</div>
+	<footer class="p-4">(footer)</footer>
+</div>
---
+++ src/routes/listings/+page.svelte
@@ -0,0 +1,9 @@
+<h3 class="h3">Listings</h3>
+<div class="grid grid-cols-1 md:grid-cols-[auto_1fr_auto]">
+	<aside class="bg-yellow-500 p-4">(search filters)</aside>
+	<main class="space-y-4 bg-green-500 p-4">
+		<p class="h-[60px] bg-purple-500 p-4">Listing 1</p>
+		<p class="h-[60px] bg-purple-500 p-4">Listing 2</p>
+		<p class="h-[60px] bg-purple-500 p-4">Listing 3</p>
+	</main>
+</div>
