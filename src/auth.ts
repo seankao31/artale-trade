457422bbd39c99697c7f6844047547665e747551
Sean Kao <seankao31@gmail.com>
2025-06-02 14:37:18 -0700

    build: install paraglide for i18n
    - modify hooks.server.ts so that handles for paraglide and auth can
      coexist
    
    Signed-off-by: Sean Kao <seankao31@gmail.com>

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,10 @@
 import { SvelteKitAuth } from '@auth/sveltekit';
 import Discord from '@auth/sveltekit/providers/discord';
 
-export const { handle, signIn, signOut } = SvelteKitAuth({
+export const {
+	handle: handleAuth,
+	signIn,
+	signOut
+} = SvelteKitAuth({
 	providers: [Discord]
 });
--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,1 +1,15 @@
-export { handle } from './auth';
+import type { Handle } from '@sveltejs/kit';
+import { sequence } from '@sveltejs/kit/hooks';
+import { handleAuth } from './auth';
+import { paraglideMiddleware } from '$lib/paraglide/server';
+
+const handleParaglide: Handle = ({ event, resolve }) =>
+	paraglideMiddleware(event.request, ({ request, locale }) => {
+		event.request = request;
+
+		return resolve(event, {
+			transformPageChunk: ({ html }) => html.replace('%paraglide.lang%', locale)
+		});
+	});
+
+export const handle = sequence(handleAuth, handleParaglide);
