4daed60b28cd968338c4ed890e4d610dc2222060
Sean Kao <seankao31@gmail.com>
2025-06-02 11:57:03 -0700

    feat: add log in button in main page
    Signed-off-by: Sean Kao <seankao31@gmail.com>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,12 @@
-<h3 class="h3">Main Page</h3>
+<div class="flex justify-center">
+	<div class="flex max-w-lg flex-col items-center">
+		<p class="p-4">
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+			labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+			laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+			voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+			non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		</p>
+		<button class="btn preset-filled-primary-500 btn-base">Log In</button>
+	</div>
+</div>
