bc2fe2a30a849545a46d6508e249b07c1a518cab
Sean Kao <seankao31@gmail.com>
2025-06-03 12:51:59 -0700

    feat: auth session switch from jwt to database
    Signed-off-by: Sean Kao <seankao31@gmail.com>

--- src/auth.ts
+++ src/auth.ts
@@ -1,4 +1,6 @@
 import { SvelteKitAuth } from '@auth/sveltekit';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import { prisma } from '$lib/prisma';
 import Discord from '@auth/sveltekit/providers/discord';
 
 export const {
@@ -6,23 +8,6 @@ export const {
   signIn,
   signOut
 } = SvelteKitAuth({
-  providers: [Discord({ authorization: { params: { scope: 'identify' } } })],
-  callbacks: {
-    async jwt({ token, profile }) {
-      // TODO: Error if not present
-      if (profile) {
-        // TODO: types
-        token.userId = profile.id;
-        token.username = profile.username;
-      }
-      return token;
-    },
-    async session({ session, token }) {
-      //@ts-expect-error token.userId type is unknown
-      session.user.id = token.userId;
-      //@ts-expect-error Property 'username' does not exist on type 'AdapterUser & User'
-      session.user.username = token.username;
-      return session;
-    }
-  }
+  adapter: PrismaAdapter(prisma),
+  providers: [Discord]
 });
