34c808a13b9210bab30b7e4e45b37949035e8a81
Sean Kao <seankao31@gmail.com>
2025-06-03 14:00:59 -0700

    feat: suppress Chrome DevTools request
    Signed-off-by: Sean Kao <seankao31@gmail.com>

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -12,4 +12,13 @@ const handleParaglide: Handle = ({ event, resolve }) =>
     });
   });
 
-export const handle = sequence(handleAuth, handleParaglide);
+const handleChromeDevTools: Handle = async ({ event, resolve }) => {
+  // Suppress well-known Chrome DevTools requests
+  if (event.url.pathname === '/.well-known/appspecific/com.chrome.devtools.json') {
+    return new Response(null, { status: 204 }); // No content
+  }
+
+  return resolve(event);
+};
+
+export const handle = sequence(handleAuth, handleParaglide, handleChromeDevTools);
