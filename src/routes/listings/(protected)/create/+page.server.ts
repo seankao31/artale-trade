628aac8efb2ec79cbf5a6ec98e89991b8ed966e1
Sean Kao <seankao31@gmail.com>
2025-06-06 15:02:32 -0700

    wip: feat: create listing entry in database
    Signed-off-by: Sean Kao <seankao31@gmail.com>

--- src/routes/listings/(protected)/create/+page.server.ts
+++ src/routes/listings/(protected)/create/+page.server.ts
@@ -4,6 +4,7 @@ import type { Actions, PageServerLoad } from './$types';
 import { superValidate, message } from 'sveltekit-superforms/server';
 import { fail } from '@sveltejs/kit';
 import { CreateListingRequestSchema } from './schema';
+import { prisma } from '$lib/prisma';
 
 export const load: PageServerLoad = async () => {
   const form = await superValidate(zod4(CreateListingRequestSchema));
@@ -16,9 +17,50 @@ export const load: PageServerLoad = async () => {
 export const actions = {
   default: async ({ request }) => {
     const form = await superValidate(request, zod4(CreateListingRequestSchema));
-    console.log(form);
 
     if (!form.valid) return fail(400, { form });
+
+    // TODO: query ItemMaster
+
+    try {
+      await prisma.listing.create({
+        data: {
+          creatorId: 'cmblcdwsn0007aofzu7kt30s4', // TODO
+          itemMasterId: 1, // TODO
+          itemName: form.data.itemName,
+          currency: form.data.priceCurrency,
+          price: form.data.priceAmount,
+          // TODO: wts, wtb
+
+          // TODO: item requirements
+          itemCategory: 'EARRING', // TODO
+
+          str: form.data.str, // TODO: this can be undefined. check if set to default value in prisma
+          dex: form.data.dex,
+          int: form.data.int,
+          luk: form.data.luk,
+
+          hp: form.data.hp,
+          mp: form.data.mp,
+          watk: form.data.watk,
+          matk: form.data.matk,
+          wdef: form.data.wdef,
+          mdef: form.data.mdef,
+          acc: form.data.acc,
+          avoid: form.data.avoid,
+          speed: form.data.speed,
+          jump: form.data.jump,
+          availableScrollSlots: form.data.availableScrollSlots,
+
+          createdAt: new Date(),
+          updatedAt: new Date(Date.now()), // TODO
+          expiresAt: new Date() // TODO
+        }
+      });
+    } catch (e) {
+      console.error(e);
+      throw e;
+    }
 
     return message(form, 'Form posted successfully!');
   }
